6506e41eae92db32ada44deddb5b40eadbab7cdf
Jayadeep KM <kmjayadeep@gmail.com>
2021-02-27 10:43:00 +0530

    made executable

--- pomo.go
+++ pomo.go
@@ -1,67 +1,71 @@
-package pomo
+package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/rwxrob/cmdtab"
 	"github.com/rwxrob/conf-go"
 )
 
-func init() {
-	x := cmdtab.New("pomo")
-	x.Summary = `sets or prints a countdown timer (with tomato)`
-	x.Usage = `[<duration>|clear]`
+var config = conf.New()
 
-	x.Description = `
-		If a Go time duration is passed then sets the pomo.start config
-		value. If no value is passed prints a tomato emoji followed by the
-		duration recmdsing. If <clear> is passed sets pomo.start to empty
-		string.`
+func usage() {
+	fmt.Println("pomo [clear|start]")
+}
 
-	// TODO let people set the emojis if different from defaults
-	// TODO add emoji for when over time by whatever amount
-	// TODO add like 5 emojis for past the deadline
-	// TODO consider adding blink support if detected in terminal
+func main() {
+	err := config.Load()
+	if err != nil {
+		panic(err)
+	}
 
-	x.Method = func(args []string) (err error) {
-		config := conf.New()
-		err = config.Load()
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		show()
+		return
+	}
+
+	switch args[0] {
+	case "clear":
+		config.SetSave("pomo.up", "")
+	case "dur":
+		// TODO validate the duration
+		config.SetSave("pomo.dur", args[1])
+	case "start":
+		// TODO detect optional duration argument
+		s := config.Get("pomo.dur")
+		if len(args) > 1 {
+			s = args[1]
+		}
+		if s == "" {
+			s = "25m"
+		}
+		config.Set("pomo.dur", s)
+		dur, err := time.ParseDuration(s)
 		if err != nil {
-			return err
+			panic(err)
 		}
+		up := time.Now().Add(dur).Format(time.RFC3339)
+		config.SetSave("pomo.up", up)
+	default:
+		usage()
+		return
+	}
+}
 
-		// [<duration>|clear]
-		if len(args) > 0 {
-			switch args[0] {
-			case "clear":
-				config.Set("pomo.up", "")
-			case "dur":
-				// TODO valid the duration
-				config.Set("pomo.dur", args[1])
-			case "start":
-				// TODO detect optional duration argument
-				s := config.Get("pomo.dur")
-				if s == "" {
-					s = "25m"
-					config.Set("pomo.dur", s)
-				}
-				dur, err := time.ParseDuration(s)
-				if err != nil {
-					return err
-				}
-				up := time.Now().Add(dur).Format(time.RFC3339)
-				config.SetSave("pomo.up", up)
-			default:
-				return x.UsageError()
-			}
-			return nil
-		}
-		up, err := time.Parse(time.RFC3339, config.Get("pomo.up"))
+func show() {
+	u := config.Get("pomo.up")
+	if u != "" {
+		up, err := time.Parse(time.RFC3339, u)
 		if err != nil {
-			return err
+			panic(err)
 		}
-		fmt.Printf("🍅 %v\n", up.Sub(time.Now()).Round(time.Second))
-		return nil
+		if up.After(time.Now()) {
+			fmt.Printf(" %v\n", up.Sub(time.Now()).Round(time.Second))
+		} else {
+			fmt.Printf(" %v\n", time.Now().Sub(up).Round(time.Second))
+		}
 	}
 }
